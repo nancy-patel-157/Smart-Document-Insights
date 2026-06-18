59e202981eea2d4b91a8b667330acd5553001757
Strongeremma <nainsipatel26@157>
2026-06-18 21:40:38 +0530

    Phase 2 Chunking Complete

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from pypdf import PdfReader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 uploaded_file = st.file_uploader(
     "Upload PDF",
@@ -12,6 +13,24 @@ if uploaded_file:
     text = ""
 
     for page in reader.pages:
-        text += page.extract_text()
+        page_text = page.extract_text()
 
-    st.write(text[:5000])
+        if page_text:
+            text += page_text
+
+    # Phase 2: Chunking
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200
+    )
+
+    chunks = splitter.split_text(text)
+
+    st.success(f"Total Chunks Created: {len(chunks)}")
+
+    st.subheader("First Chunk")
+    st.write(chunks[0])
+
+    st.subheader("Second Chunk")
+    if len(chunks) > 1:
+        st.write(chunks[1])
